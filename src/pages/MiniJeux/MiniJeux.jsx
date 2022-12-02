a579795137c106b10a370956cb79309f07b1dcfb
lorenzo-italiano <lorenzo.italiano34@gmail.com>
2022-12-02 05:39:18 +0100

    fixed Quizz css + integrated MiniJeux.jsx

--- src/pages/MiniJeux/MiniJeux.jsx
+++ src/pages/MiniJeux/MiniJeux.jsx
@@ -1,8 +1,24 @@
-import "../../styles/pages/minijeux.scss" 
+import "../../styles/pages/minijeux.scss"
+import Button from '@mui/material/Button';
+import { useNavigate } from "react-router-dom"
 
 const MiniJeux = () => {
+
+	const navigate = useNavigate()
+
 	return(
-		<></>
+		<div id="cardContainer">
+			<div className="card secondary">
+				<h1 className="text">Jeu du pendu</h1>
+				<p className="text">Jouer au jeu du pendu tout en apprenant de nouveaux mots relatif au sida !</p>
+				<Button variant="contained">Jouer</Button>
+			</div>
+			<div className="card secondary">
+				<h1 className="text">Quizz</h1>
+				<p className="text">Apprenez et cultivez vous en vous amusant au travers du quizz !</p>
+				<Button variant="contained" onClick={() => { navigate("/minijeux/quizz")} }>Jouer</Button>
+			</div>
+		</div>
 	)
 }
 
